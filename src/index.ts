123ce9f7fbc3ce5dccbfddef6778b2b8da5be471
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-06-11 23:46:52 -0300

    V 0.2.0

--- src/index.ts
+++ src/index.ts
@@ -18,29 +18,31 @@ const bot = async() => {
   const data = {
     user: process.env.TINY_USER || "",
     password: process.env.TINY_PASSWORD || "",
-    lifeActionValue: process.env.HTML_LIFE_ACTION_VALUE || "",
     selectorLife: process.env.HTML_SELECTOR_LIFE || "",
-    selectorPotion: process.env.HTML_SELECTOR_POTION || ""
   }
 
-  await page.frames()[0].focus("#app > div.app-container.d-flex.justify-content-center.align-items-center.h-100 > div.mt-5.d-flex.justify-content-center.align-items-middle.flex-column.w-50 > div:nth-child(3) > input")
+  await page.frames()[0]?.focus("#app > div.app-container.d-flex.justify-content-center.align-items-center.h-100 > div.mt-5.d-flex.justify-content-center.align-items-middle.flex-column.w-50 > div:nth-child(3) > input")
   
   //@ts-ignore
   data.user.split('').forEach( async (letter) => await page.keyboard.press(letter))
 
   //@ts-ignore
-  await page.frames()[0].focus("#app > div.app-container.d-flex.justify-content-center.align-items-center.h-100 > div.mt-5.d-flex.justify-content-center.align-items-middle.flex-column.w-50 > div:nth-child(4) > input")
+  await page.frames()[0]?.focus("#app > div.app-container.d-flex.justify-content-center.align-items-center.h-100 > div.mt-5.d-flex.justify-content-center.align-items-middle.flex-column.w-50 > div:nth-child(4) > input")
   //@ts-ignore
   data.password.split('').forEach( async (letter) => await page.keyboard.press(letter))
 
   //@ts-ignore
-  await page.frames()[0].focus("#app > div.app-container.d-flex.justify-content-center.align-items-center.h-100 > div.mt-5.d-flex.justify-content-center.align-items-middle.flex-column.w-50 > button.btn.btn-outline-primary.mb-3")
+  await page.frames()[0]?.focus("#app > div.app-container.d-flex.justify-content-center.align-items-center.h-100 > div.mt-5.d-flex.justify-content-center.align-items-middle.flex-column.w-50 > button.btn.btn-outline-primary.mb-3")
   //@ts-ignore
   await page.keyboard.press('Enter')
 
   await page.waitForTimeout(1000)
 
   const response = await page.evaluate(async (data) => {
+
+    function sleep(ms:number) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
     
     let currentLife = 0
     let timeOff = 0   
@@ -85,7 +87,38 @@ const bot = async() => {
       return potions
     }
 
-    const books = () => {
+    const shopPotion1280 = document.querySelector('#tab_shop > ul > li:nth-child(6)')
+    const accountName = document.querySelector('#app > div.app-container.d-flex.justify-content-center.align-items-center.h-100 > div.container.h-100.mt-5 > div > div > div > div:nth-child(2) > div.card.shadow-sm.mb-3 > div > div.d-flex.justify-content-between > h5.shine')
+    
+    //@ts-ignore
+    accountName?.innerHTML = 'TinyBot'
+
+    const getDungeons = () => {
+      const list = [...document.querySelector('#app > div.app-container.d-flex.justify-content-center.align-items-center.h-100 > div.container.h-100.mt-5 > div > div > div > div:nth-child(1) > div.card.shadow-sm.mb-3 > div.card-header.bg-white.d-flex.justify-content-between.align-items-center > ul')?.querySelectorAll('li')]
+      
+      return list.map(dungeon => ({
+        name: dungeon.textContent.trim(),
+        click: () => dungeon.querySelector('a').click()
+      }))
+    }
+
+    const getMob = () => {
+      const details = document.querySelector('#app > div.app-container.d-flex.justify-content-center.align-items-center.h-100 > div.container.h-100.mt-5 > div > div > div > div:nth-child(1) > div.card.shadow-sm.mb-3 > div.card-body.d-flex.justify-content-center.w-100.flex-column > div.align-items-center.justify-content-center.d-flex.flex-column')
+      const [currentLife, totalLife] = details?.querySelector('div')?.querySelectorAll('span')[1]?.textContent?.trim().split('/') || []
+
+      return {
+        currentLife: Number(currentLife), 
+        totalLife: Number(totalLife),
+        name: details?.textContent?.trim() || ''
+      }
+    }
+
+    const addAtk = () => {
+      const atkElement = document.querySelector('#collapse_attributes > div:nth-child(1)')?.querySelector('button')
+      if(atkElement) atkElement.click()
+    }
+
+    const getBooks = () => {
       const inventory = document.querySelector('.list-group')?.querySelectorAll("li")
 
       //@ts-ignore
@@ -126,14 +159,14 @@ const bot = async() => {
 
       books.push(course)
 
-      const indexRebirth = books.findIndex(book => book.name.indexOf('Rebirth') > -1)
+      const indexRebirth = books.findIndex(book => book?.name.indexOf('Rebirth') > -1)
 
       books.splice(indexRebirth, 1)
 
       return books
     }
 
-    const startBot = () => {
+    const startBot = async () => {
       life = 
         document
           .querySelector(data.selectorLife)
@@ -142,11 +175,24 @@ const bot = async() => {
         //@ts-ignore
         ?.textContent?.split('/') || ["0", "0"]
 
-      books().every(book => {
+      const books = getBooks()
+
+      books[0] ? books.every(book => {
         book.consume()
         return false
-      })
-          
+      }) : {}
+
+      addAtk()
+
+      const mob = getMob()
+
+      // if(mob.name.indexOf('Absolute God Amon') > -1){
+      //   const dungeons = getDungeons()
+      //   dungeons[7].click()
+      //   await sleep(1000)
+      //   dungeons[8].click()
+      // }
+      
       potions().every(potion => {
         life = 
         document
@@ -158,6 +204,11 @@ const bot = async() => {
 
         const diffLife = Number(total) - Number(current)
 
+        if(potion.name.indexOf('Absolute') > -1 && potion.amount < 5000){
+          shopPotion1280?.querySelector('button')?.click()
+        }
+
+        if(Number(total) - Number(current) > 15000)
         if(diffLife >= potion.cure && potion.amount > 0) {
           potion.consume()
           return false
@@ -166,14 +217,13 @@ const bot = async() => {
         return true
       })
 
-      if(currentLife === Number(current)){
+      if(currentLife === mob.currentLife){
         timeOff++
       }else{
         timeOff = 0
       }
 
-      currentLife = Number(current)
-
+      currentLife = mob.currentLife
     }
 
     async function waitUntil() {
@@ -181,7 +231,7 @@ const bot = async() => {
         //@ts-ignore
         const interval = setInterval(startBot, 500);
         const final = setInterval(() => {
-          if(timeOff > 60000000) {
+          if(timeOff > 100) {
             timeOff = 0
             clearInterval(interval)
             clearInterval(final)
